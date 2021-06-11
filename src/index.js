63e0dbeeae364fa43994be2b89373fce6789967c
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-06-11 20:05:34 +0300

    убрал togle

--- src/index.js
+++ src/index.js
@@ -21,12 +21,16 @@ function chengeTheme() {
     const checked = refs.chekboxInput.checked;
     // console.log(checked);
 
-    refs.choseBody.classList.toggle(Theme.DARK);
-    refs.choseBody.classList.toggle(Theme.LIGHT);
+    
   
     if (checked === true) {        
         localStorage.setItem('themeIsNow', Theme.DARK);
+refs.choseBody.classList.add(Theme.DARK);
+    refs.choseBody.classList.remove(Theme.LIGHT);
+
     } else if (checked === false) {
+        refs.choseBody.classList.remove(Theme.DARK);
+        refs.choseBody.classList.add(Theme.LIGHT);
         localStorage.removeItem('themeIsNow');              
         localStorage.setItem('themeIsNow', refs.choseBody.classList);
     }
