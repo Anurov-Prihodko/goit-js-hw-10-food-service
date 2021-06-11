2a18ce24facdb4416f39a9f277a690184c70a286
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-06-11 19:49:19 +0300

    Все работает

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
+'use strict';
+import mainMarkup from './templates/markup.hbs';
+import menu from './menu.json';
 import './sass/main.scss';
+
+const refs = {
+    chekboxInput: document.querySelector('#theme-switch-toggle'),
+    choseBody: document.querySelector('body'),
+    markup: document.querySelector('.menu.js-menu'),
+}
+
+refs.markup.insertAdjacentHTML('beforeend', mainMarkup(menu));
+refs.chekboxInput.addEventListener('change', chengeTheme);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+function chengeTheme() {
+    const checked = refs.chekboxInput.checked;
+    // console.log(checked);
+
+    refs.choseBody.classList.toggle(Theme.DARK);
+    refs.choseBody.classList.toggle(Theme.LIGHT);
+  
+    if (checked === true) {        
+        localStorage.setItem('themeIsNow', Theme.DARK);
+    } else if (checked === false) {
+        localStorage.removeItem('themeIsNow');              
+        localStorage.setItem('themeIsNow', refs.choseBody.classList);
+    }
+};
+
+if (localStorage.getItem('themeIsNow') === Theme.DARK) {
+    refs.choseBody.classList.add(Theme.DARK);
+    refs.chekboxInput.checked = true;
+};
